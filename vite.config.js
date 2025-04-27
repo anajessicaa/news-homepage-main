efbe943c0dfe82f1e9940256307eb170740f372e
anajessicaa <anajessica.dev@gmail.com>
2025-04-27 15:36:15 -0400

    configurações de deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/news-homepage-main-Frontend-Mentor",
+  base: "/news-homepage-main",
 })
